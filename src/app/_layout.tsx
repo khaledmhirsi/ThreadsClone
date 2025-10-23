f4248f8c16a51a3f1c64ce2d003ee81d5869fe39
Khaled M'hirsi <schoolkha@hotmail.com>
2025-10-23 08:12:53 +0300

    File Structure + Nativewind setup

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import '../../global.css';
+
+import { Slot } from 'expo-router';
+
+import { ThemeProvider, DarkTheme } from '@react-navigation/native';
+
+const myTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: 'white',
+    card: '#101010',
+  },
+};
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={myTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
+}
